dad70cc6319be5035162a52a0e637901118461a2
Adam <akpgreentree@gmail.com>
2020-09-04 15:27:12 -0700

    Updated docstrings

--- fastq_reader.py
+++ fastq_reader.py
@@ -1,9 +1,19 @@
 import gzip as gz
+import io
 from itertools import zip_longest
 
 class FASTQError(Exception): pass
 
 class FASTQRecord:
+
+    '''Data structure for one FASTQ read
+    
+    Attributes:
+        name, sequence, quality: from the three meaningful lines in 
+            a fastq file.
+    
+    '''
+
     def __init__(self, name: str, sequence: str, quality: str):
         self.name = name
         self.sequence = sequence
@@ -18,7 +28,10 @@ class FASTQRecord:
         return (s)
 
 class FASTQReader:
-    def __init__(self, file):
+
+    '''Iterates through a fastq file, returning records'''
+
+    def __init__(self, file: io.TextIOBase):
         self._file = file
 
     def __iter__(self):
@@ -41,6 +54,14 @@ class FASTQReader:
 
 
 class PairedFASTQRecord:
+
+    '''Data structure for one paired-end FASTQ read
+    
+    Attributes: 
+        read1, read2: FASTQ records
+
+    '''
+
     def __init__(self, read1: FASTQRecord, read2: FASTQRecord):
         if read1.name.split(' ')[0] != read2.name.split(' ')[0]:
             raise FASTQError("Mismatched paired-end records")
@@ -51,7 +72,12 @@ class PairedFASTQRecord:
 
 class PairedFASTQReader:
 
-    def __init__(self, read1_file, read2_file):
+    '''
+    Iterates through paired FASTQ files and returns 
+    paired records.
+    '''
+
+    def __init__(self, read1_file: io.TextIOBase, read2_file: io.TextIOBase):
         self._reader1 = FASTQReader(read1_file)
         self._reader2 = FASTQReader(read2_file)
         self._zipped_reader = zip_longest(self._reader1, self._reader2)
