656fae186c73b662a22d35423b33b165d333337b
Adam <akpgreentree@gmail.com>
2020-09-09 11:52:41 -0700

    Slight reorg and increased documentation

---
+++ fastq_stats/__main__.py
@@ -0,0 +1,5 @@
+from . import run_analysis
+
+if __name__ == "__main__":
+    cli = run_analysis.CLI()
+    cli.main()
--- fastq_stats/run_analysis.py
+++ fastq_stats/run_analysis.py
@@ -1,10 +1,21 @@
+'''Tools for running full analysis'''
+
 from typing import List
+import argparse
 import fastq_reader
 import stats_collectors
 import stats_output
 
 
 class AnalysisRunner:
+
+    '''Given a set of parameters, runs a particular analysis.
+    
+    Parameters:
+        * Input files
+        * Stats calculators
+        * Outputs
+    '''
 
     def __init__(self, read1_path: str, read2_path: str,
                  stats: List[stats_collectors.PairedStatsCalculator],
@@ -23,3 +34,52 @@ class AnalysisRunner:
 
             for output in self._outputters:
                 output.output_stats(stats)
+
+
+class CLI:
+
+    '''Command Line Interface for running analyses'''
+    
+    def __init__(self):
+        self.parser = argparse.ArgumentParser(
+            description = "Collect stats on FASTQ files.")
+        self.parser.add_argument("input_r1",
+                                 help = "FASTQ file for read 1.")
+        self.parser.add_argument("input_r2",
+                                 help = "FASTQ file for read 1.")
+        self.parser.add_argument("--seq", nargs = "+",
+                                 help = "Sequences to match against.")
+        self.parser.add_argument(
+            "--quality", 
+            action="store_false",
+            help = "Option to report average per-base quality scores.")
+        self.parser.add_argument('--csv',
+                                 help = "Output csv file path.")
+
+    def _setup_runner(self):
+        stats = []
+        if self.args.seq:
+            stats.append(stats_collectors.SequenceMatcher(self.args.seq))
+        if self.args.quality:
+            stats.append(stats_collectors.QualityAverager())
+        if not stats:
+            raise ValueError("No statistics specified.")
+        
+        outputs = []
+        if self.args.csv:
+            outputs.append(stats_output.CsvWriter(self.args.csv))
+        if not outputs:
+            raise ValueError("No outputs specified.")
+
+        self.runner = AnalysisRunner(self.args.read1_path,
+                                     self.args.read2_path,
+                                     stats, outputs)
+
+    def main(self, args=None):
+        if args:
+            self.args = self.parser.parse_args(args)
+        else:
+            self.args = self.parser.parse_args()
+
+        self._setup_runner()
+        self.runner.run_analysis()
